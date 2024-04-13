c22569b3827c976575a6bfc655f3f8bdabcbccf5
UzairAhmed87 <uzairahmed1574@gmail.com>
2024-04-13 15:26:41 +0500

    added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const close = document.getElementById("close");
+const mainImg=document.getElementById("mainImg")
+const smallImg = document.getElementsByClassName("small-img")
 
 
 if (bar) {
@@ -14,3 +16,16 @@ if (close) {
         nav.style.right = "-300px"
     })
 }
+
+smallImg[0].addEventListener("click",()=>{
+    mainImg.src = smallImg[0].src
+})
+smallImg[1].addEventListener("click",()=>{
+    mainImg.src = smallImg[1].src
+})
+smallImg[2].addEventListener("click",()=>{
+    mainImg.src = smallImg[2].src
+})
+smallImg[3].addEventListener("click",()=>{
+    mainImg.src = smallImg[3].src
+})
